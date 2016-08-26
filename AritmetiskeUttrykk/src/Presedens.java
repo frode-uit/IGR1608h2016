f3c7a988ad0367d5356ce963cd3ce1f14adaa188
Frode Naesje <fn@hin.no>
2016-08-26 10:20:35 +0200

    Initial commit

---
+++ AritmetiskeUttrykk/src/Presedens.java
@@ -0,0 +1,16 @@
+
+public class Presedens {
+
+	public static void main(String[] args) {
+		int heltallA = 21;
+		int heltallB = 6;
+		int heltallC = 3;
+		int heltallD = 1;
+		
+		int resultat1 = heltallA - heltallB / heltallC;
+		int resultat2 = (heltallA - heltallB) / heltallC;
+		System.out.println(resultat1);
+		System.out.println(resultat2);
+		
+	}
+}
---
+++ Kapittel1/src/ShowSyntaxError.java
@@ -0,0 +1,10 @@
+
+public class ShowSyntaxError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+
+	}
+
+}
